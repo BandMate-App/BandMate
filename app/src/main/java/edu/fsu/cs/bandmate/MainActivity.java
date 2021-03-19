1bcfb12255bc373d628f79cb228bb3b5da212514
wrh17b <74798057+wrh17b@users.noreply.github.com>
2021-03-19 15:58:42 -0400

    Initial Commit

---
+++ app/src/main/java/edu/fsu/cs/bandmate/MainActivity.java
@@ -0,0 +1,14 @@
+package edu.fsu.cs.bandmate;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
